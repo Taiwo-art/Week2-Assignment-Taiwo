2d0351ff03589355b73a04c4130cc2d8488fc364
Taiwo Lambo <lmb.taiwo@gmail.com>
2022-08-31 23:57:56 +0100

    new files added

---
+++ promise.js
@@ -0,0 +1,9 @@
+
+        const promise1 = fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>res.json())
+        .then(res=> {
+            let result = res.map(post =>`<li>${post.title}</li>`);
+
+        return document.body.innerHTML = result.join('');
+        })
+
